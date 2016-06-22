fbc1ffdcb500870df14968bf0812d9c45a902d0d
Krykun-Evgeny <krykun.evgeny@gmail.com>
2016-06-22 11:21:17 +0300

    Add project

---
+++ algorithms_task_2/Divisor/Divisor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("List of divisors: ");
+            for (int i = 1; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
